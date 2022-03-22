c8cf6b453bebc073abff3b710d16b0456a4bbef7
uziel78 <rune.husa78@gmail.com>
2022-03-22 16:14:01 +0100

    test

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,13 @@
+import Head from "next/head";
 import "../styles/globals.scss";
 import Layout from "../components/layout/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
