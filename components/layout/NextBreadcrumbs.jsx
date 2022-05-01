d26b5d41588bcf0740c1c8cb23baae733900182c
uziel78 <rune.husa78@gmail.com>
2022-05-01 16:06:33 +0200

    breadcrumbs-fix

--- components/layout/NextBreadcrumbs.jsx
+++ components/layout/NextBreadcrumbs.jsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import styles from "./NextBreadcrumbs.module.scss";
 
 export default function NextBreadcrumbs() {
-  return <Breadcrumbs styles={styles.breadcrumbs} aria-label="breadcrumb" />;
+  return <div>Breadcrumbs</div>;
 }
